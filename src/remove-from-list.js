5956fc65e71b14453844c6756bdfb96928cac9c0
SlavaNekrish <vnekrish@mail.ru>
2023-12-25 15:55:38 +0300

    feat: implement stack

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (l == null) {
+    return l;
+  }
+  while (l.value == k) {
+    l = l.next;
+  }
+  thisNode = l;
+  nextNode = thisNode.next;
+  while (nextNode != null) {
+    if (nextNode.value == k) {
+      thisNode.next = nextNode.next;
+      // No more nodes, ie last node was to be removed
+      if (thisNode.next == null) break;
+    }
+    thisNode = thisNode.next;
+    nextNode = thisNode.next;
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
